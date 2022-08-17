a278fc5759fc57182e56f8776260f34e4d7696f9
MrEmmanuel <sejesoo@gmail.com>
2022-08-17 19:48:53 +0200

    Added failing tests

---
+++ src/test/java/GameOfLifeTests.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameOfLifeTests {
+
+
+    @Test
+    @DisplayName("This test should test Any live cell with fewer than two live neighbors dies")
+    public void anyLiveCellWithFewerThanTwoliveNeighborsDiesTest(){
+    }
+
+    @Test
+    @DisplayName("This test should test Any live cell with two or three live neighbors lives on to the next generation")
+    public void anyLiveCellWithTwoOrThreeLiveNeighborsLivesTest(){
+
+    }
+
+    @Test
+    @DisplayName("Test should test Any live cell with more than three live neighbors dies")
+    public void anyLiveCellMoreWithThanThreeLiveNeighborsDiesTest(){
+
+    }
+    @Test
+    @DisplayName("This test should Any dead cell with exactly three live neighbors becomes a live cell")
+    public void anyDeadCellWithThreeLiveNeighborsBecomesALiveCellTest(){
+
+    }
+}
